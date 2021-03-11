cb92589d45decf651557aa61edd20b356d43ad22
jiggy2204 <jignacio2204@gmail.com>
2021-03-10 23:03:32 -0500

    bookmark server update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
+
+const bookmarkRouter = require("./bookmark-router");
+const { NODE_ENV } = require("./config");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const winston = require("winston");
-
-const { NODE_ENV } = require("./config");
 const { restart } = require("nodemon");
 
 const app = express();
@@ -15,41 +15,6 @@ const morganOption = NODE_ENV === "production";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-
-const bookmark = [
-  {
-    id: 1,
-    title: "JiggyART",
-    description: "The homespace of a contract developer",
-    rating: 3,
-    url: "https://jiggyart.org",
-  },
-  {
-    id: 2,
-    title: "YouTube",
-    description: "The place of videos since forever",
-    rating: 4,
-    url: "https://youtube.com",
-  },
-];
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: "info.log",
-    }),
-  ],
-});
-
-if (NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-}
 
 //VALIDATE API_TOKEN
 app.use(function validateBearerToken(req, res, next) {
@@ -73,17 +38,14 @@ app.use(function errorHandler(error, req, res, next) {
     console.error(error);
     response = { message: error.message, error };
   }
-
-  //GET root page, send back 'Hello, world!' on web page
-  app.get("/", (req, res) => {
-    res.send("Hello, world!");
-  });
-
-  app.get("/bookmark", (res, req) => {
-    res.json(bookmark);
-  });
-
-  res.status(500).json(response);
 });
+
+//GET root page, send back 'Hello, world!' on web page
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+//USE app after validateBearerToken so no one can use beforehand
+app.use(bookmarkRouter);
 
 module.exports = app;
---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmark = [
+  {
+    id: 1,
+    title: "My Website",
+    description: "My personal site",
+    url: "https://jiggyart.org",
+    rating: 5,
+  },
+  {
+    id: 2,
+    title: "Instagram",
+    description: "My picutre site",
+    url: "https://instagram.com",
+    rating: 3,
+  },
+];
+
+module.exports = { bookmark };
